e6614d42b4a1ce923382ec1939761a0b3e175d67
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-09-07 14:29:13 -0700

    calculator created

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,20 @@
 // This program calculates a person's height in feet (') and inches (").
+
+  #include <iostream>
+
+    int main()
+  {
+    // Ask for height in inches
+  std::cout << "Enter person's height in inches: ";
+  int  inchheight;
+  std::cin >> inchheight;
+
+  // Convert the height to feet and inches
+  double feet = inchheight / 12;
+  int inches = inchheight % 12;
+
+std::cout << "This person is " << feet <<"'" << inches <<"\" in height.\n";
+
+  return 0;
+  }
+// modulus operator %
