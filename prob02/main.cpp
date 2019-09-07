0a0250081115d8abd4bb2646f12af925379607e6
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-09-07 13:37:32 -0700

    errors found and corrected

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -2,19 +2,19 @@
 
 #include <iostream>
 
-int main()
+  int main()
 {
-  // one gallon of paint covers 340 square feet
+  // One gallon of paint covers 340 square feet.
+
   int coverage = 340;
-   int height = 6;
-    int length = 100,
+  int height = 6;
+  int width = 100;
 
-  // calculate how many gallons of paint it will take to cover
-  // the area with 2 coats of paint
-  int area=height*width*2;
-    int gallons = area / coverage
+  // Calculate how many gallons of paint it will take to cover
+  // the area with 2 coats of paint.
+  int area = height * width * 2;
+  int gallons = area / coverage;
 
-  std::cout << "It will take " << coverage << " gallons of paint./n";
-
+  std::cout << "It will take " << gallons << " gallons of paint.\n";
   return 0;
 }
